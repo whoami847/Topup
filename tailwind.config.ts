2400f66f196792e94a4adfc1e05ef714406190c1
Who Am i <whoami999901@gmail.com>
2025-07-09 20:03:49 +0000

    tes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,23 @@ export default {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        md: '2rem',
+        lg: '4rem',
+        xl: '6rem',
+        '2xl': '8rem',
+      },
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1400px',
+        '2xl': '1600px',
+      },
+    },
     extend: {
       fontFamily: {
         body: ['Inter', 'sans-serif'],
